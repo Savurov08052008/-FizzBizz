a312329c549b9afa0f00ebe3d5b069b310c2f7b8
Savurov08052008 <126753020+Savurov08052008@users.noreply.github.com>
2023-03-06 18:42:55 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+# # DARS REJASI ##
+
+# 1. ----
+# 2. ----
+# 3. ----
+# 4. ----
+# 5. ----
+# 6. ----
+# 7. ----
+
+# ##############################################################################################
+
+
+# son = int(input("Sonni kiriting"))
+# # Agar
+# if Son / 3:
+#     print("Fizz")
+# elif Son / 5:
+#     print("Buzz")
+# else Sonlar / 5 and 3:
+#     print("FizzBuzz")
+
+
+a = int(input("Firstnumber:"))
+b = int(input("Secondnumber:"))
+if oper == '+':
+    print(Firstnumber + Secondnumber)
+elif oper == '-'
+    print(Firstnumber - Secondnumber)
+elif oper == '*'
+    print(Firstnumber * Secondnumber)
+elif oper == '/'
+    print(Firstnumber / Secondnumber)
+elif oper == '**'
+    print(Firstnumber ** Secondnumber)
+elif oper == '***'
+    print(Firstnumber *** Secondnumber)
+elif:
+    print("Error")
+
